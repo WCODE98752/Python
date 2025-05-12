6327ba18873e1a70d40413c3d122043c5beec766
Faraz Abbasi <wcode472@gmail.com>
2025-05-12 17:56:39 +0500

    Greastest one

---
+++ Greatest one.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+if a > b and a > c:
+    print("a is the greatest")
+elif b > a and b > c:
+    print("b is the greatest")
+else:
+    print("c is the greatest")
