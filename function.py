c7a3edc319a92a482af5d3fbaf5074c2219bb8de
Faraz Abbasi <wcode472@gmail.com>
2025-06-02 13:59:15 +0500

    Sum function

---
+++ function.py
@@ -0,0 +1,11 @@
+
+
+
+
+def calculator(a , b):
+    c = a + b
+    print('This is the sum',c)
+    
+a = int(input("Enter a first num: "))
+b = int(input("Enter a second num: "))
+calculator(a, b)     
